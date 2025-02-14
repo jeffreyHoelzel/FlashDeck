54c2ff741e9c3b54b992e9c7b04bd9e6892cd329
jeffreyHoelzel <jmh2338@nau.edu>
2025-02-14 09:51:08 -0700

    Added three necessary classes for quizlet style app

--- service/app.py
+++ service/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
+
 
 app = Flask(__name__)
 CORS(app)
@@ -9,6 +10,18 @@ CORS(app)
 def test():
     return jsonify({"message": "Hello, world!"}), 200
 
+@app.route("/create_new_deck", methods=["GET"])
+def create_deck():
+    if request.method == "GET":
+        pass
+
+@app.route("/edit_existing_deck", methods=["GET"])
+def edit_deck():
+    pass
+
+@app.route("/quiz_deck", methods=["GET"])
+def start_quiz():
+    pass
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
